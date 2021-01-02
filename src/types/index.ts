5467c19431b321547b74a06e392a25ff547f0b04
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2021-01-02 14:34:10 -0800

    Created types.ts

---
+++ src/types/index.ts
@@ -0,0 +1,16 @@
+export enum TileType {
+  INACTIVE,
+  ACTIVE,
+  ROADBLOCK,
+  START,
+  FINISH,
+  PORTAL,
+}
+
+export enum TooltipTypes {
+  FLAG = "FLAG",
+  ROAD = "ROAD",
+  CLEAR = "CLEAR",
+  ROADBLOCK = "ROADBLOCK",
+  PORTAL = "PORTAL",
+}
