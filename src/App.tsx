a26c9f8831da7177789563ae51cdd1d6c9e230a8
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2021-01-02 13:17:44 -0800

    Created core components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Grid from "./components/Grid";
+import Header from "./components/Header";
+import Slider from "./components/Slider";
+import Tooltip from "./components/Tooltip";
 
 function App() {
-  return <div>MazeBuilder</div>;
+  return (
+    <div>
+      <Header />
+      <div>
+        <Tooltip />
+        <Grid />
+        <Slider />
+      </div>
+    </div>
+  );
 }
 
 export default App;
