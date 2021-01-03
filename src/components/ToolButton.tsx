4887834fe83d35ff86030b7f9d1d4393e94e8bc7
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2021-01-03 00:59:14 -0800

    Added onClick handlers to tool buttons

--- src/components/ToolButton.tsx
+++ src/components/ToolButton.tsx
@@ -1,15 +1,26 @@
 import React from "react";
+import { TileType } from "../types";
 
 type ToolButtonProps = {
   iconUrl: string;
   label: string;
   width: number;
+  type: TileType;
+  onClickHandler: (newType: TileType) => void;
 };
 
 function ToolButton(props: ToolButtonProps) {
-  const { iconUrl, label, width } = props;
+  const { iconUrl, label, width, type, onClickHandler } = props;
+
+  const handleButtonClick = () => {
+    onClickHandler(type);
+  };
+
   return (
-    <button className="flex flex-col justify-center mb-3 min-w-full hover:bg-yellow-200">
+    <button
+      className="flex flex-col justify-center mb-3 min-w-full hover:bg-yellow-200"
+      onClick={handleButtonClick}
+    >
       <img className="m-auto" src={iconUrl} alt={label} width={width} />
       <span className="m-auto text-base">{label}</span>
     </button>
--- src/components/Toolbox.tsx
+++ src/components/Toolbox.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-// import { TileType } from "../types";
-import { connect, ConnectedProps } from "react-redux";
-// import { changeTool } from "../store/actions/tooltipActions";
+import { TileType } from "../types";
 import ToolButton from "./ToolButton";
+import { connect, ConnectedProps } from "react-redux";
+import { changeTool } from "../store/actions/tooltipActions";
 
 const connector = connect();
 type PropsFromRedux = ConnectedProps<typeof connector>;
@@ -10,12 +10,10 @@ type PropsFromRedux = ConnectedProps<typeof connector>;
 type TooltipProps = PropsFromRedux;
 
 function Toolbox(props: TooltipProps) {
-  // const { dispatch } = props;
-  // const handleClick = () => {
-  //   // TODO: change val based on what is click on
-  //   dispatch(changeTool(TileType.BLOCKADE));
-  //   console.log("Changed tool");
-  // }
+  const { dispatch } = props;
+  const handleToolClick = (newType: TileType) => {
+    dispatch(changeTool(newType));
+  };
 
   return (
     <div className="bg-yellow-300 rounded-r-3xl px-4 my-20 flex flex-col justify-center max-h-full">
@@ -23,21 +21,29 @@ function Toolbox(props: TooltipProps) {
         iconUrl="https://cdn4.iconfinder.com/data/icons/transportation-solid-style/24/road-256.png"
         label="Road"
         width={60}
+        type={TileType.ROAD}
+        onClickHandler={handleToolClick}
       />
       <ToolButton
         iconUrl="https://cdn4.iconfinder.com/data/icons/objects-1-3/50/82-256.png"
         label="Roadblock"
         width={60}
+        type={TileType.ROADBLOCK}
+        onClickHandler={handleToolClick}
       />
       <ToolButton
         iconUrl="https://cdn3.iconfinder.com/data/icons/flat-icons-web/40/Pin_Flag-256.png"
         label="Flag"
         width={60}
+        type={TileType.FLAG}
+        onClickHandler={handleToolClick}
       />
       <ToolButton
         iconUrl="https://cdn3.iconfinder.com/data/icons/education-801/64/Eraser-Remove-Paper-Pencil-256.png"
         label="Erase"
         width={60}
+        type={TileType.BLOCKADE}
+        onClickHandler={handleToolClick}
       />
     </div>
   );
