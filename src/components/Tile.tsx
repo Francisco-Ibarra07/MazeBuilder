23a0f8e2ef68056aad94b32bd64a761d4f3d0293
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2021-01-02 18:04:39 -0800

    Updated Tile component props

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,14 +1,24 @@
 import React from "react";
-import { TileType } from "../types";
+import { Location, TileType } from "../types";
 
 type TileProps = {
   type: TileType;
+  location: Location;
+  onClickHandler: (type: TileType, location: Location) => void;
 };
 
 function Tile(props: TileProps) {
-  const { type } = props;
+  const { type, location, onClickHandler } = props;
 
-  return <div>Tile type: {type}</div>;
+  const handleClick = () => {
+    onClickHandler(type, location);
+  };
+
+  return (
+    <div className="tile-container" onClick={handleClick}>
+      <p>Tile type: {type}</p>
+    </div>
+  );
 }
 
 export default Tile;
