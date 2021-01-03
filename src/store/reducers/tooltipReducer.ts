7c9d0868c4c456b0624e8f9c71a4a0c84a524b58
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2021-01-03 15:53:13 -0800

    Added same type check in tooltipReducer

--- src/store/reducers/tooltipReducer.ts
+++ src/store/reducers/tooltipReducer.ts
@@ -6,7 +6,11 @@ const initialState = TileType.ROAD;
 const tooltipReducer = (state = initialState, action: ToolActionTypes) => {
   switch (action.type) {
     case CHANGE_TOOL:
-      return action.payload;
+      if (state !== action.payload) {
+        return action.payload;
+      } else {
+        return state;
+      }
     default:
       return state;
   }
