107c44fd4c10bf7b82673efb4fb0deb365bc24ee
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2021-01-02 18:05:01 -0800

    Updated Grid component to display all tiles stored in redux store

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { TileType } from "../types";
+import { Location, TileType } from "../types";
 import { connect, ConnectedProps } from "react-redux";
 import { RootState } from "../store/reducers/rootReducer";
 import { updateTile } from "../store/actions/gridActions";
@@ -8,26 +8,33 @@ import Tile from "./Tile";
 // Connect Redux
 const mapStateToProps = (state: RootState) => ({
   grid: state.grid,
+  tooltip: state.tooltip,
 });
 const connector = connect(mapStateToProps);
 type PropsFromRedux = ConnectedProps<typeof connector>;
 type GridProps = PropsFromRedux;
 
 function Grid(props: GridProps) {
-  const { dispatch, grid } = props;
+  const { dispatch, grid, tooltip } = props;
 
-  const handleClick = () => {
-    dispatch(updateTile(TileType.FINISH, { row: 1, col: 1 }));
-    console.log("Updated tile");
+  const tileClickHandler = (type: TileType, location: Location) => {
+    // Only update if clicked tile isn't same as current tooltip
+    if (type !== tooltip) {
+      dispatch(updateTile(tooltip, location));
+    }
   };
 
   return (
     <div>
-      <button onClick={handleClick}>Update tile</button>
-      {grid.map((row, index) => (
-        <div className="row" key={`row-${index}`}>
-          {row.map((value, index) => (
-            <Tile key={`col-${index}`} type={value} />
+      {grid.map((row, rowIndex) => (
+        <div className="tile-row" key={`row-${rowIndex}`}>
+          {row.map((value, colIndex) => (
+            <Tile
+              key={`col-${colIndex}`}
+              type={value}
+              location={{ row: rowIndex, col: colIndex }}
+              onClickHandler={tileClickHandler}
+            />
           ))}
         </div>
       ))}
