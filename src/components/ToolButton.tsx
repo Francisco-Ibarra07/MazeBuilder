31b507d2647a056a519edf807697220f7d0a5957
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2021-01-03 15:53:27 -0800

    Added disable prop to ToolButton component

--- src/components/ToolButton.tsx
+++ src/components/ToolButton.tsx
@@ -6,11 +6,12 @@ type ToolButtonProps = {
   label: string;
   width: number;
   type: TileType;
+  disable?: boolean;
   onClickHandler: (newType: TileType) => void;
 };
 
 function ToolButton(props: ToolButtonProps) {
-  const { iconUrl, label, width, type, onClickHandler } = props;
+  const { iconUrl, label, width, type, disable, onClickHandler } = props;
 
   const handleButtonClick = () => {
     onClickHandler(type);
@@ -20,9 +21,10 @@ function ToolButton(props: ToolButtonProps) {
     <button
       className="flex flex-col justify-center mb-3 min-w-full hover:bg-yellow-200"
       onClick={handleButtonClick}
+      disabled={disable}
     >
       <img className="m-auto" src={iconUrl} alt={label} width={width} />
-      <span className="m-auto text-base">{label}</span>
+      <span className="m-auto text-lg">{label}</span>
     </button>
   );
 }
