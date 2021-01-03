8c78ef4993e34bda255079cf7a188e28ca8a733e
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2021-01-02 20:05:27 -0800

    Added minor css styles to header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Header() {
-  return <div>Title: MazeBuilder</div>;
+  return (
+    <div className="pt-2 pb-5 flex justify-center">
+      <div className="bg-yellow-300 p-3 rounded-lg text-2xl font-mono">
+        <h1>Maze Builder</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
