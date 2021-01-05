914e392fe14c3fad0732c6ea6a2fccafcda978e3
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2021-01-04 20:13:10 -0800

    Created new util function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,11 @@
 export function isInBounds(grid: any[], row: number, col: number): boolean {
   return grid && row >= 0 && row < grid.length && col >= 0 && col < grid[row].length;
 }
+
+export function deepCopy(grid: any[][]) {
+  return grid.map((row) => {
+    return row.map((item) => {
+      return item;
+    });
+  });
+}
