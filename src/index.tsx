f91f8bd9bf70369fafaba60f4b8908109289c91f
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2021-01-07 14:10:13 -0800

    Imported initial chakra ui configurations

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,22 @@ import { Provider } from "react-redux";
 import store from "./store";
 import "./index.css";
 import App from "./App";
+import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "Open Sans",
+    body: "Titillium Web",
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ColorModeScript />
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
