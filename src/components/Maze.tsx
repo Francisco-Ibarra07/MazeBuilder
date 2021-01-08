a8ebaaf89260fef740789704761dee8f4574f7c9
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2021-01-07 23:03:53 -0800

    Added redux code back into Maze component

--- src/components/Maze.tsx
+++ src/components/Maze.tsx
@@ -1,7 +1,5 @@
 import {
   Flex,
-  Box,
-  Grid,
   Slider,
   SliderTrack,
   SliderFilledTrack,
@@ -9,11 +7,50 @@ import {
   Tooltip,
   Button,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import MazeGrid from "./MazeGrid";
 import ToolBox from "./ToolBox";
+import { connect, ConnectedProps } from "react-redux";
+import { RootState } from "../store/reducers/rootReducer";
+import { resetFlags } from "../store/actions/flagActions";
+import { expandGrid, shrinkGrid, solveGrid } from "../store/actions/gridActions";
 
-function Maze() {
+// Connect Redux
+const mapStateToProps = (state: RootState) => ({
+  grid: state.grid,
+  flagLocations: state.flagLocations,
+});
+const connector = connect(mapStateToProps);
+type PropsFromRedux = ConnectedProps<typeof connector>;
+type MazeProps = PropsFromRedux;
+
+function Maze(props: MazeProps) {
+  const { grid, flagLocations, dispatch } = props;
+  const [isSolving, setIsSolving] = useState(false);
+
+  const handleDecrement = () => {
+    dispatch(shrinkGrid());
+    dispatch(resetFlags());
+  };
+
+  const handleIncrement = () => {
+    dispatch(expandGrid());
+    dispatch(resetFlags());
+  };
+
+  const handleSolve = () => {
+    if (flagLocations.length !== 2) {
+      console.log("Not enough flags on the board");
+      return;
+    }
+
+    setIsSolving(true);
+    const start = flagLocations[0];
+    const end = flagLocations[1];
+    dispatch(solveGrid(start, end));
+    setIsSolving(false);
+  };
+
   return (
     <Flex flexDirection="column" justifyContent="center" alignItems="center" mt={6}>
       <Flex w="100%" flexDirection="column" justifyContent="center" alignItems="center">
@@ -34,11 +71,17 @@ function Maze() {
         </SliderThumb>
       </Slider>
 
-      <Button bg="purple.200" mt={1}>
+      <Button
+        mt={1}
+        variant="outline"
+        onClick={handleSolve}
+        isLoading={isSolving}
+        isDisabled={flagLocations.length !== 2}
+      >
         Solve!
       </Button>
     </Flex>
   );
 }
 
-export default Maze;
+export default connector(Maze);
