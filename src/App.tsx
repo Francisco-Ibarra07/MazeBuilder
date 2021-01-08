34db8606073a7299a13ad7c19b8185f0561603ce
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2021-01-08 00:37:20 -0800

    Removed SubHeading component from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import { Box } from "@chakra-ui/react";
 import TopHeading from "./components/TopHeading";
-import SubHeading from "./components/SubHeading";
 import Maze from "./components/Maze";
 
 function App() {
   return (
     <Box>
       <TopHeading />
-
-      {/* <SubHeading /> */}
 
       <Maze />
     </Box>
