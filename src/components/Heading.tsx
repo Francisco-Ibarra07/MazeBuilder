90b4d6a22f5e714e35504c2b9c2ce9e64a64091b
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2021-01-07 16:27:27 -0800

    Replaced many 'Box' components with 'Flex'

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,36 +1,32 @@
 import React from "react";
-import { Box, Image, Link, Text } from "@chakra-ui/react";
+import { Box, Flex, Image, Link, Text } from "@chakra-ui/react";
 
 function Heading() {
   return (
     <Box bg="blue.300" p={4} borderColor="blue.300" borderBottom="1px">
-      <Box display="flex" justifyContent="space-between">
+      <Flex justifyContent="space-between">
         {/* Image + Name */}
-        <Box display="flex">
+        <Flex>
           <Image
             src="https://lh6.googleusercontent.com/FI-VrYVCsSLDa1SWxWGZxQPbeaZZojxAW--6vk3tZxoXUQq9NXsPJXako_JVXAbk0nGmpQw9LtnO2uYnU-5SUpP1wrMYqjSBYWeJld42FRowXWv0-ZyV-65QuXaRkJVA77IEsZIz"
             alt="Francisco"
             w="60px"
             mr={3}
           />
-          <Text
-            color="white"
-            fontSize="2xl"
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-          >
-            Francisco Ibarra
-          </Text>
-        </Box>
+          <Flex alignItems="center" justifyContent="center">
+            <Text color="white" fontSize="2xl">
+              Francisco Ibarra
+            </Text>
+          </Flex>
+        </Flex>
         {/* Project title */}
-        <Box display="flex" alignItems="center" justifyContent="center">
+        <Flex alignItems="center" justifyContent="center">
           <Text color="white" fontSize="2xl">
             Maze Builder
           </Text>
-        </Box>
+        </Flex>
         {/* Media links */}
-        <Box display="flex" alignItems="center">
+        <Flex alignItems="center">
           <Link href="https://linkedin.com/in/franciscoibarra/" isExternal>
             <Image
               src="https://cdn4.iconfinder.com/data/icons/social-icons-16/512/LinkedIn-256.png"
@@ -46,8 +42,8 @@ function Heading() {
               boxSize="30px"
             />
           </Link>
-        </Box>
-      </Box>
+        </Flex>
+      </Flex>
     </Box>
   );
 }
