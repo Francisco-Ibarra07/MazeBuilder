373d562842e87db845f8d88f964f3e6515c753ea
Francisco Ibarra <Francisco.ibarra@sjsu.edu>
2021-01-07 22:07:18 -0800

    Added redux code back into ToolBox and ToolButton components

--- src/components/ToolBox.tsx
+++ src/components/ToolBox.tsx
@@ -1,13 +1,33 @@
-import React from "react";
-import { Flex, Text } from "@chakra-ui/react";
-import ToolButton from "./ToolButton";
+import React, { useState } from "react";
 import { TileType } from "../types";
+import ToolButton from "./ToolButton";
+import { Flex, Text } from "@chakra-ui/react";
+import { connect, ConnectedProps } from "react-redux";
+import { RootState } from "../store/reducers/rootReducer";
+import { changeTool } from "../store/actions/tooltipActions";
 import pathIcon from "../resources/icons/path-icon.png";
 import flagIcon from "../resources/icons/flag-icon.png";
 import eraserIcon from "../resources/icons/eraser-icon.png";
 import roadblockIcon from "../resources/icons/roadblock-icon.png";
 
-function ToolBox() {
+const mapStateToProps = (state: RootState) => ({
+  flagLocations: state.flagLocations,
+});
+const connector = connect(mapStateToProps);
+type PropsFromRedux = ConnectedProps<typeof connector>;
+type TooltipProps = PropsFromRedux;
+
+function ToolBox(props: TooltipProps) {
+  const { dispatch, flagLocations } = props;
+  const [activeTool, setActiveTool] = useState<TileType>(TileType.ROAD);
+
+  const handleToolClick = (newType: TileType) => {
+    if (newType !== activeTool) {
+      dispatch(changeTool(newType));
+      setActiveTool(newType);
+    }
+  };
+
   return (
     <Flex
       w={3 / 4}
@@ -29,6 +49,8 @@ function ToolBox() {
           fontSize="xl"
           hoverColor="blue.200"
           type={TileType.ROAD}
+          isActive={activeTool === TileType.ROAD}
+          onClickHandler={handleToolClick}
         />
         <ToolButton
           iconSrc={roadblockIcon}
@@ -37,6 +59,8 @@ function ToolBox() {
           fontSize="xl"
           hoverColor="blue.200"
           type={TileType.ROADBLOCK}
+          isActive={activeTool === TileType.ROADBLOCK}
+          onClickHandler={handleToolClick}
         />
         <ToolButton
           iconSrc={flagIcon}
@@ -45,6 +69,9 @@ function ToolBox() {
           fontSize="xl"
           hoverColor="blue.200"
           type={TileType.FLAG}
+          isDisabled={flagLocations.length === 2}
+          isActive={activeTool === TileType.FLAG}
+          onClickHandler={handleToolClick}
         />
         <ToolButton
           iconSrc={eraserIcon}
@@ -53,10 +80,12 @@ function ToolBox() {
           fontSize="xl"
           hoverColor="blue.200"
           type={TileType.BLOCKADE}
+          isActive={activeTool === TileType.BLOCKADE}
+          onClickHandler={handleToolClick}
         />
       </Flex>
     </Flex>
   );
 }
 
-export default ToolBox;
+export default connector(ToolBox);
--- src/components/ToolButton.tsx
+++ src/components/ToolButton.tsx
@@ -11,16 +11,25 @@ type ToolButtonProps = {
   type: TileType;
   isDisabled?: boolean;
   isActive?: boolean;
-  onClickHandler?: (newType: TileType) => void;
+  onClickHandler: (newType: TileType) => void;
 };
 
 function ToolButton(props: ToolButtonProps) {
+  const handleClick = () => {
+    props.onClickHandler(props.type);
+  };
+
   return (
     <Flex
       as="button"
       mx={5}
+      cursor={props.isDisabled ? "not-allowed" : ""}
+      bg={props.isDisabled ? "#e5e5e5" : ""}
+      border={props.isActive && !props.isDisabled ? "4px solid #4ADE80" : ""}
       borderRadius="md"
       _hover={{ bg: props.hoverColor }}
+      _focus={{ outline: "none" }}
+      onClick={handleClick}
       flexDirection="column"
       alignItems="center"
     >
